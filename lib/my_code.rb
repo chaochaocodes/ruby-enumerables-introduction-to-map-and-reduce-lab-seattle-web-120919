f2d9bcaa0510e053b69e7748a39964d4a5bee596
honorable-exception-1170 <aliciachao@gmail.com>
2019-12-08 22:17:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,12 +1,12 @@
 # My Code here....
 def map_to_negativize(source_array)
-neg = []
+neg_array = []
 i = 0
   while i < source_array.length do
     neg_array << source_array[i] * -1
     i += 1
   end
-  neg
+  neg_array
 end
 
 def map_to_no_change(source_array)
